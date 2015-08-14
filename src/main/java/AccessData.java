433a68958d6c5e624d818f48e021b97c1b1b7db2
unknown <kyak@pc-qa.livatek.com>
2015-08-14 12:43:19 +0300

    without passwords

--- src/main/java/AccessData.java
+++ src/main/java/AccessData.java
@@ -2,12 +2,11 @@
  * Created by kyak on 07.08.2015.
  */
 public interface AccessData {
-    String TESTURL = "https://test.hansoftx.com/login";
-    String WRONGTESTLOGIN = "livatek.user15@ukr.net";
-    String WRONGTESTPASSWORD = "2015user02";
+    String TESTURL = "";
+    String WRONGTESTLOGIN = "";
+    String WRONGTESTPASSWORD = "";
 
-    String TESTLOGIN = "livatek.user14@ukr.net";
-    String TESTPASSWORD = "2015user02";
-
+    String TESTLOGIN = "";
+    String TESTPASSWORD = "";
 
 }
